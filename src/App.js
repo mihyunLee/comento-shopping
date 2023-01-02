726b0af843dbc97cd25a5fa145b5982d1d7fb82c
mihyunLee <mihy621@gmail.com>
2023-01-02 14:31:03 +0900

    ✅feature: 페이지와 라우팅 연결하기 (#6)

--- src/App.js
+++ src/App.js
@@ -1,14 +1,17 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "./App.css";
+import Basket from "./pages/Basket";
+import Home from "./pages/Home";
+import ProductDetail from "./pages/ProductDetail";
 
 function App() {
   return (
     <div className="App">
       <BrowserRouter basename="comento-shopping">
         <Routes>
-          <Route path="/" />
-          <Route path="/product/:productId" />
-          <Route path="/basket" />
+          <Route path="/" element={<Home />} />
+          <Route path="/product/:productId" element={<ProductDetail />} />
+          <Route path="/basket" element={<Basket />} />
         </Routes>
       </BrowserRouter>
     </div>
