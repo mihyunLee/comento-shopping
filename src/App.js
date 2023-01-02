00df9b37c23a69a0aa9b8a6ad98dcf855df44915
mihyunLee <mihy621@gmail.com>
2023-01-02 14:21:55 +0900

    ✅feature: App.js에 라우팅 적용 (#6)

--- src/App.js
+++ src/App.js
@@ -1,21 +1,16 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <BrowserRouter basename="comento-shopping">
+        <Routes>
+          <Route path="/" />
+          <Route path="/product/:productId" />
+          <Route path="/basket" />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
