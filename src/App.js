776dd8ca989c2917041c16345deca4fa8f6933d4
mihyunLee <mihy621@gmail.com>
2023-01-02 17:33:03 +0900

    ♻️refactor: App 컴포넌트 정렬 제거 및 App.css 삭제 (#9)

--- src/App.js
+++ src/App.js
@@ -1,20 +1,17 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import "./App.css";
 import Basket from "./pages/Basket";
 import Home from "./pages/Home";
 import ProductDetail from "./pages/ProductDetail";
 
 function App() {
   return (
-    <div className="App">
-      <BrowserRouter basename="comento-shopping">
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/product/:productId" element={<ProductDetail />} />
-          <Route path="/basket" element={<Basket />} />
-        </Routes>
-      </BrowserRouter>
-    </div>
+    <BrowserRouter basename="comento-shopping">
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/product/:productId" element={<ProductDetail />} />
+        <Route path="/basket" element={<Basket />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
