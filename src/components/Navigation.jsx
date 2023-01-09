70e65a3bcd755314b98d1d5d891af31cb1b90056
Nor <51310674+mihyunLee@users.noreply.github.com>
2023-01-09 17:44:50 +0900

    🩹fix: Navigation 컴포넌트 구조와 디자인 변경 (#16)

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -2,12 +2,33 @@ import styled from "styled-components";
 
 const Navigation = () => {
   // TODO: ProductDetail 페이지일 시, 화살표 모양 추가
-  return <NavagationTitle>코멘토 쇼핑</NavagationTitle>;
+  return (
+    <StyledNavigation>
+      <span>&lt;</span>
+      <h3>코멘토 쇼핑</h3>
+      <span>&nbsp;</span>
+    </StyledNavigation>
+  );
 };
 
-const NavagationTitle = styled.h3`
-  font-size: 18px;
-  text-align: center;
+const StyledNavigation = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+
+  span {
+    padding-left: 31px;
+  }
+
+  span:nth-child(1) {
+    cursor: pointer;
+    visibility: hidden;
+  }
+
+  h3 {
+    font-size: 18px;
+    text-align: center;
+  }
 `;
 
 export default Navigation;
