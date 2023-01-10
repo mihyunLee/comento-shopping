beda37152c00c702ff4353523cea156e010b9177
Nor <51310674+mihyunLee@users.noreply.github.com>
2023-01-10 20:10:24 +0900

    💄design: Review 컴포넌트 디자인 (#18)
    - Review 컴포넌트 디자인
    - ProductDetailView의 상세 이미지를 DOM element에서 styled-components로 변경

--- src/components/ProductDetailView.jsx
+++ src/components/ProductDetailView.jsx
@@ -3,9 +3,9 @@ import Review from "./Review";
 
 const ProductDetailView = ({ detailImg, name, tabMenu, reviews }) => {
   return (
-    <StyledProductDetailView>
+    <StyledProductDetailView tabMenu={tabMenu}>
       {tabMenu === 0 ? (
-        <img src={detailImg} alt={name + " 상품 설명"} />
+        <DetailImg src={detailImg} alt={name + " 상품 설명"} />
       ) : (
         reviews.map((review) => <Review key={review.id} {...review} />)
       )}
@@ -15,21 +15,21 @@ const ProductDetailView = ({ detailImg, name, tabMenu, reviews }) => {
 
 const StyledProductDetailView = styled.div`
   width: 100%;
-  height: 825px;
+  height: ${(props) => (props.tabMenu === 0 ? "825px" : "330px")};
 
   overflow-y: scroll;
 
   &::-webkit-scrollbar {
     display: none;
   }
+`;
 
-  img {
-    width: 342px;
+const DetailImg = styled.img`
+  width: 342px;
 
-    margin: 0 24px;
+  margin: 0 24px;
 
-    object-fit: cover;
-  }
+  object-fit: cover;
 `;
 
 export default ProductDetailView;
--- src/components/Review.jsx
+++ src/components/Review.jsx
@@ -1,17 +1,66 @@
+import styled from "styled-components";
+
 const Review = ({ ...review }) => {
   return (
-    <div>
-      <div>
+    <StyledReview>
+      <UserSection>
         <img src={review.profileImg} alt={"프로필 이미지"} />
-        <div>
-          <span>{review.star}</span>
-          <span>{review.user}</span>
-          <span>{review.date}</span>
-        </div>
-      </div>
-      <div>{review.content}</div>
-    </div>
+        <span>{review.star}</span>
+        <span>
+          {review.user} | {review.date}
+        </span>
+      </UserSection>
+      <ContentSection>
+        <span>{review.content}</span>
+      </ContentSection>
+    </StyledReview>
   );
 };
+
+const StyledReview = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+
+  width: 341px;
+  height: 84px;
+
+  box-sizing: border-box;
+
+  margin-bottom: 16px;
+  margin-left: 24px;
+  margin-right: 24px;
+
+  padding: 5px 10px;
+
+  background-color: #fafafa;
+`;
+
+const UserSection = styled.div`
+  display: grid;
+  grid-template-columns: 0.5fr 3fr;
+  grid-template-rows: repeat(2, 1fr);
+  align-items: center;
+
+  img {
+    grid-row: 1/ -1;
+
+    width: 40px;
+    height: 40px;
+
+    border-radius: 30%;
+  }
+
+  span {
+    font-size: 12px;
+    color: #a7a7a7;
+  }
+`;
+
+const ContentSection = styled.div`
+  margin-top: 5px;
+
+  font-size: 14px;
+`;
 
 export default Review;
